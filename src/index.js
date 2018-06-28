852eaa2db09101d0c36fa4d33a697a61a2818968
Justin Barca <justin.barca@points.com>
2018-06-28 14:55:18 -0400

    add computed props feature

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import deepFreeze from 'deep-freeze-strict';
 import cloneDeep from 'lodash-es/cloneDeep';
 import get from 'lodash-es/get';
 import isString from 'lodash-es/isString';
+import mapValues from 'lodash-es/mapValues';
 
 let state = {};
 const subscribers = new Set();
+const computedProps = {};
 
 const applyJSONFilter = (callback, filter) => (next, prev) => {
   next = filter(next);
@@ -22,17 +24,30 @@ const notify = (newState, prevState) => {
   }
 };
 
+const freezeWithComputedProps = (aState) => {
+  const computed = mapValues(computedProps, computeFn => computeFn(aState));
+  return deepFreeze({ ...aState, computed });
+};
+
 const commit = (newState) => {
   const prev = state;
-  state = deepFreeze(newState);
-  notify(state, prev);
+  state = newState;
+  notify(freezeWithComputedProps(state), freezeWithComputedProps(prev));
 };
 
 const copyState = () => cloneDeep(state);
 
 export const action = fn => (...args) => fn({ commit, state: copyState() }, ...args);
 
-export const getState = () => state;
+export const getState = () => freezeWithComputedProps(state);
+
+export const registerComputedProp = (key, computeFn) => {
+  computedProps[key] = computeFn;
+};
+
+export const deregisterComputedProp = (key) => {
+  delete computedProps[key];
+};
 
 export const subscribe = (callback, filter) => {
   if (isString(filter)) {
