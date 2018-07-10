0af240e3c16bc36359f98c6a50a527d869c8ed7f
andornaut <andornaut@users.noreply.github.com>
2018-07-10 07:31:23 -0400

    Remove unnecessary jest options

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,6 @@
 // https://facebook.github.io/jest/docs/en/configuration.html
 
 module.exports = {
-  clearMocks: true,
-  rootDir: 'test',
   testEnvironment: 'node',
   testMatch: ['**/test/test_*.?(m)js?(m)'],
 };
