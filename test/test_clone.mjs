a1b728ca6afe7ea6e2a8add8aac48218d5c825ce
andornaut <andornaut@users.noreply.github.com>
2018-07-09 21:30:08 -0400

    Rename variable

--- test/test_clone.mjs
+++ test/test_clone.mjs
@@ -11,30 +11,30 @@ const createOriginal = () => ({
 test('mutating original array property does not change clone', () => {
   const original = createOriginal();
 
-  const clonedA = clone(original);
+  const cloned = clone(original);
 
   original.array.push(3);
   expect(original.array.length).toBe(3);
-  expect(clonedA.array.length).toBe(2);
+  expect(cloned.array.length).toBe(2);
 });
 
 test('mutating original date property does not change clone', () => {
   const original = createOriginal();
 
-  const clonedA = clone(original);
+  const cloned = clone(original);
 
   const originalTime = original.b.date.getTime();
   original.b.date.setTime(0);
   expect(original.b.date.getTime()).toBe(0);
-  expect(clonedA.b.date.getTime()).toBe(originalTime);
+  expect(cloned.b.date.getTime()).toBe(originalTime);
 });
 
 test('mutating original string property does not change clone', () => {
   const original = createOriginal();
 
-  const clonedA = clone(original);
+  const cloned = clone(original);
 
   original.b.c = 'CHANGED';
   expect(original.b.c).toBe('CHANGED');
-  expect(clonedA.b.c).toBe('C');
+  expect(cloned.b.c).toBe('C');
 });
