d8419ce16d639fea877514c3fb9eba40c6bcc06a
andornaut <andornaut@users.noreply.github.com>
2018-09-23 21:08:44 -0400

    Update package-lock

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://facebook.github.io/jest/docs/en/configuration.html
-
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/test/test_*.?(m)js?(m)'],
-};
