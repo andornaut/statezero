b3a226d2cd0a518e94aa8a1b3c46a1b4e8af7dfa
andornaut <andornaut@users.noreply.github.com>
2018-09-24 10:13:11 -0400

    Cleanup

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,4 @@
-const webpackConfig = require('./webpack.config');
+const webpackConfig = require('./webpack.config')({}, { mode: 'development' });
 
 const TEST_FILES = 'test/index.js';
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,30 @@ const path = require('path');
 const srcPath = path.join(__dirname, 'src');
 const testPath = path.join(__dirname, 'test');
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: [srcPath],
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+module.exports = (env, argv) => {
+  const mode = argv ? argv.mode : 'production';
+  return {
+    mode,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: [srcPath],
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
           },
         },
-      },
-      {
-        test: /\.js$/,
-        include: [srcPath, testPath],
-        enforce: 'pre',
-        loader: 'eslint-loader',
-      },
-    ],
-  },
-  plugins: [new CleanWebpackPlugin(['dist'])],
+        {
+          test: /\.js$/,
+          include: [srcPath, testPath],
+          enforce: 'pre',
+          loader: 'eslint-loader',
+        },
+      ],
+    },
+    plugins: [new CleanWebpackPlugin(['dist'])],
+  };
 };
