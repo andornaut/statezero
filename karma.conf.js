8b518ded7a28b5059688385f202262d156447078
andornaut <andornaut@users.noreply.github.com>
2020-03-13 09:06:01 -0400

    Simplify karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,14 @@
 const webpackConfig = require('./webpack.config')({}, { mode: 'development' });
 
-const TEST_FILES = 'test/index.js';
+const TEST_FILES = 'test/test_*.js';
 
 module.exports = (config) => {
   const autoWatch = !!config.WATCH;
   config.set({
     autoWatch,
-    browserNoActivityTimeout: 60000,
-    browsers: ['ChromeNoSandbox'],
-    captureConsole: true,
-    customLaunchers: {
-      ChromeNoSandbox: {
-        base: autoWatch ? 'Chrome' : 'ChromeHeadless',
-        flags: ['--disable-gpu', '--disable-web-security', '--no-sandbox'],
-      },
-    },
-    files: [TEST_FILES],
+    browsers: ['ChromeHeadless'],
+    files: [{ pattern: TEST_FILES }],
     frameworks: ['mocha', 'sinon-chai'],
-    // logLevel: config.LOG_DEBUG,
     preprocessors: {
       [TEST_FILES]: ['webpack'],
     },
